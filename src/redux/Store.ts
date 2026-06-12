b849d570f35fa75a8ef2535e8e5995f7f776662b
Luca Müller <lucamueller97@gmx.de>
2026-06-12 11:47:46 +0200

    Remove redux-thunk middleware from store configuration

--- src/redux/Store.ts
+++ src/redux/Store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { thunk } from 'redux-thunk';
 import RootReducer from './reducers/RootReducer';
 
 const store = configureStore({
   reducer: RootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk)
 });
 
 export default store;
