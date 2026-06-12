f87c0ae22b2e7403d10f8e9545bc833d9158d8f5
Luca Müller <lucamueller97@gmx.de>
2026-06-12 14:17:15 +0200

    Simplify GamePartWebPart by removing React and Redux dependencies; implement minimal test rendering

--- src/webparts/gamePart/GamePartWebPart.tsx
+++ src/webparts/gamePart/GamePartWebPart.tsx
@@ -1,46 +1,12 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
 import { Version } from '@microsoft/sp-core-library';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { Provider } from 'react-redux';
-import store from '../../redux/Store';
-import { GamePartComponent } from './components/GamePartComponent';
-import { IGamePartComponentProperties } from './IGamePartComponentProperties';
 
-// Wird ausgefuehrt, sobald das Bundle vom Browser geladen wird. Erscheint dieser
-// Log NICHT in der Konsole, wird das Bundle gar nicht geladen (z.B. localhost-
-// Zertifikat nicht vertraut) - dann liegt es nicht am Code.
-console.log('[GamePart] WebPart-Bundle geladen');
+console.log('[GamePart] MINIMAL-TEST: Bundle geladen');
 
 export default class GamePartWebPart extends BaseClientSideWebPart<Record<string, never>> {
-  protected async onInit(): Promise<void> {
-    console.log('[GamePart] onInit start');
-    try {
-      const result = await super.onInit();
-      console.log('[GamePart] onInit done');
-      return result;
-    } catch (error) {
-      console.error('[GamePart] onInit error:', error);
-      throw error;
-    }
-  }
-
   public render(): void {
-    console.log('[GamePart] render start');
-    try {
-      const element: React.ReactElement<IGamePartComponentProperties> = React.createElement(
-        GamePartComponent,
-        { Context: this.context }
-      );
-      ReactDom.render(<Provider store={store}>{element}</Provider>, this.domElement);
-      console.log('[GamePart] render done');
-    } catch (error) {
-      console.error('[GamePart] render error:', error);
-    }
-  }
-
-  protected onDispose(): void {
-    ReactDom.unmountComponentAtNode(this.domElement);
+    console.log('[GamePart] MINIMAL-TEST: render');
+    this.domElement.innerHTML = '<h2 style="color:green">GamePart MINIMAL-TEST funktioniert</h2>';
   }
 
   protected get dataVersion(): Version {
